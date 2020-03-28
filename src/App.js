6c9e1d58bb41e839c0ffdb4714b34f97fcce32ed
Jason Qu <fishinsea2014@gmail.com>
2020-03-28 15:39:40 +1300

    Create sotre, install dependencies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from "react-redux";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 import './App.css';
+import {store}  from "./store";
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <PostForm></PostForm>
       <Posts />
       
     </div>
+    </Provider>
   );
 }
 
